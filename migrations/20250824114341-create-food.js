be98c40a931a9144a017f59bd16e36ff7ef932e6
kamalesh .B <kamalesh944555@gmail.com>
2025-08-24 18:38:28 +0530

    swagger issue fix

--- migrations/20250824114341-create-food.js
+++ migrations/20250824114341-create-food.js
@@ -5,7 +5,6 @@ module.exports = {
     await queryInterface.createTable('Food', {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4
--- migrations/20250824114409-create-cart.js
+++ migrations/20250824114409-create-cart.js
@@ -5,7 +5,6 @@ module.exports = {
     await queryInterface.createTable('Carts', {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4
