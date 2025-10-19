fed4a790a86e564867530733b8f6466c4393419c
Kishon-coder <kishonkrishnan0103@gmail.com>
2025-10-19 11:22:23 +0530

    Firebse

--- config/firebase.js
+++ config/firebase.js
@@ -1,24 +1,24 @@
-const admin = require("firebase-admin");
+// const admin = require("firebase-admin");
 
-const path = require("path");
-require("dotenv").config();
+// const path = require("path");
+// require("dotenv").config();
 
-let serviceAccount;
+// let serviceAccount;
 
-try {
-  // If you are using a JSON file (recommended)
-  serviceAccount = require(path.join(__dirname, "serviceAccount.json"));
-} catch (err) {
-  // OR you can use .env variables instead
-  serviceAccount = {
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  };
-}
+// try {
+//   // If you are using a JSON file (recommended)
+//   serviceAccount = require(path.join(__dirname, "serviceAccount.json"));
+// } catch (err) {
+//   // OR you can use .env variables instead
+//   serviceAccount = {
+//     project_id: process.env.FIREBASE_PROJECT_ID,
+//     client_email: process.env.FIREBASE_CLIENT_EMAIL,
+//     private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+//   };
+// }
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
-module.exports = admin;
+// module.exports = admin;
