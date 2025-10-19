482f7042b47f94b6e807c95e5015e6dc635bf435
Kishon-coder <kishonkrishnan0103@gmail.com>
2025-10-19 11:30:47 +0530

    New

--- utils/firebaseOTP.js
+++ utils/firebaseOTP.js
@@ -1,21 +1,21 @@
-// const admin = require("../config/firebase");
+const admin = require("../config/firebase");
 
-// async function sendFirebaseOTP(fcmToken, otp) {
-//   try {
-//     const message = {
-//       notification: {
-//         title: "OTP Verification",
-//         body: `Your verification code is ${otp}`
-//       },
-//       token: fcmToken, // FCM token of the device
-//     };
+async function sendFirebaseOTP(fcmToken, otp) {
+  try {
+    const message = {
+      notification: {
+        title: "OTP Verification",
+        body: `Your verification code is ${otp}`
+      },
+      token: fcmToken, // FCM token of the device
+    };
 
-//     const response = await admin.messaging().send(message);
-//     return response;
-//   } catch (err) {
-//     console.error("Firebase send OTP error:", err);
-//     return null;
-//   }
-// }
+    const response = await admin.messaging().send(message);
+    return response;
+  } catch (err) {
+    console.error("Firebase send OTP error:", err);
+    return null;
+  }
+}
 
-// module.exports = { sendFirebaseOTP };
+module.exports = { sendFirebaseOTP };
