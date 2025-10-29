583589355df8a4be3d389eab3bc8ce69129d1a2d
Kishon-coder <kishonkrishnan0103@gmail.com>
2025-10-29 19:05:41 +0530

    new

--- migrations/20250831105409-create-cuisine.js
+++ migrations/20250831105409-create-cuisine.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Cuisines', {
+    await queryInterface.createTable('cuisines', {
       id: {
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4, // Sequelize handles UUID generation
@@ -24,6 +24,6 @@ module.exports = {
      
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Cuisines');
+    await queryInterface.dropTable('cuisines');
   }
 };
