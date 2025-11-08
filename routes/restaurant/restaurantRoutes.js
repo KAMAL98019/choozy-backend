420ab781aeded8b96fcd5e6509df5edca33f86a6
Kishon-coder <kishonkrishnan0103@gmail.com>
2025-11-08 14:01:36 +0530

    New changes

--- routes/restaurant/restaurantRoutes.js
+++ routes/restaurant/restaurantRoutes.js
@@ -20,7 +20,7 @@ router.post('/forgot-password/resend-otp', ctrl.resendOTP);
 
 // Public listing (customers can see restaurants)
 router.get('/',authenticateUser,ctrl.list);
-router.get('/:id',authenticateUser,ctrl.getById);
+router.get('/:id',authenticateRestaurant,ctrl.getById);
 router.get('/:id/foods',authenticateUser, ctrl.getRestaurantFoods);
 
 // ============= PROTECTED ROUTES (JWT Required) =============
