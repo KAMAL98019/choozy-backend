ea088c4eb8c09dd21427113b02ca60c4f633c36d
Kishon-coder <kishonkrishnan0103@gmail.com>
2025-11-08 17:42:21 +0530

    New

--- routes/admin/cuisineRoutes.js
+++ routes/admin/cuisineRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const ctrl = require('../../controllers/admin/cuisineController');
 const { authenticateAdmin } = require("../../middlewares/authMiddleware");
+const { authenticateRestaurant } = require("../../middlewares/authMiddleware");
 
-router.post('/',authenticateAdmin, ctrl.create);
-router.get('/',authenticateAdmin, ctrl.getAll);
+router.post('/', ctrl.create);
+router.get('/',authenticateRestaurant, ctrl.getAll);
 router.get('/:id',authenticateAdmin, ctrl.getById);
 router.put('/:id',authenticateAdmin, ctrl.update);
 router.delete('/:id',authenticateAdmin, ctrl.remove);
